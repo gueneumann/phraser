0f984a23fb6e11aa0885a4eff0048f95c6a8bd86
Günter Neumann <neumann@dfki.de>
2015-04-20 10:10:50 +0200

    ADD

---
+++ phraser/src/de/dfki/lt/tools/WikiPediaList2Nemex.java
@@ -0,0 +1,14 @@
+package de.dfki.lt.tools;
+
+/**
+ * This class will transform a given Wikipedia list as defined in Ratonov and Roth, CoNLL 09.
+ * A list is just a list of MWL without any further information.
+ * However, the filename gives a type.
+ * Shen scanning a list, one needs to apply some filters, e.g., spam entries that contain
+ * Wikipedia meta information
+ * @author gune00
+ *
+ */
+public class WikiPediaList2Nemex {
+
+}
